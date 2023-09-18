33e68f047fa7ac36b637bb6670f12e5b4740dee9
alirezayn <alireza.yn@outlook.com>
2023-09-18 19:46:50 +0330

    update userProfile.jsx

--- components/userProfile/UserProfile.jsx
+++ components/userProfile/UserProfile.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./userProfile.module.scss";
-import {  Tab, Tabs } from "react-bootstrap";
+import {  Nav, Tab, Tabs } from "react-bootstrap";
 const UserProfile = () => {
+  const [key, setKey] = useState('profile');
+  console.log(key)
   return (
     <div className={`${styles.mainContainer}`}>
       <div className={`${styles.profile}`}>
-             
+             <div className={`${styles.right}`}>
+
+             </div>
+             <div className={`${styles.left}`}></div>
       </div>
       <div className={`${styles.profileInfo}`}>
-      <Tabs
+      {/* <Tabs
       defaultActiveKey="profile"
-      id="justify-tab-example"
-      className="mb-5 mt-3"
+      id="controlled-tab-example"
+      // className="mb-5 mt-3"
+      onSelect={(e)=>setKey(e)}
       style={{borderBottom:"none"}}
     >
       <Tab eventKey="profile" title="پروفایل">
@@ -26,7 +32,27 @@ const UserProfile = () => {
       <Tab eventKey="contact" title="Contact" >
         Tab content for Contact
       </Tab>
-    </Tabs>
+    </Tabs> */}
+      <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+      {/* <Row> */}
+        {/* <Col sm={3}> */}
+          <Nav variant="dark" className="flex-row">
+            <Nav.Item>
+              <Nav.Link eventKey="first">Tab 1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="second">Tab 2</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        {/* </Col> */}
+        {/* <Col sm={9}> */}
+          <Tab.Content>
+            <Tab.Pane eventKey="first">First tab content</Tab.Pane>
+            <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
+          </Tab.Content>
+        {/* </Col> */}
+      {/* </Row> */}
+    </Tab.Container>
     
       </div>
     </div>
