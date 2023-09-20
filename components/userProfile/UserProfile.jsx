8df405c9368938d1321c560b2610198fb630846a
alirezayn <alireza.yn@outlook.com>
2023-09-20 15:16:13 +0330

    update userProfile.jsx

--- components/userProfile/UserProfile.jsx
+++ components/userProfile/UserProfile.jsx
@@ -1,11 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./userProfile.module.scss";
-import { Nav, Tab, Table, Tabs } from "react-bootstrap";
+import {
+  Form,
+  InputGroup,
+  Nav,
+  NavDropdown,
+  Tab,
+  Table,
+  Tabs,
+} from "react-bootstrap";
 import Image from "next/image";
 import { useSelector } from "react-redux";
+import { profile, tabIcon } from "./userData";
+
+
 const UserProfile = () => {
   const user = useSelector((state) => state.user);
-  const [key, setKey] = useState("profile");
+  const [key, setKey] = useState("پروفایل");
+  const [width, setWidth] = useState(0);
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWidth(window.innerWidth);
+    });
+  }, []);
+
   const setNavKey = (e) => {
     setKey(e.target.title);
   };
@@ -15,6 +33,7 @@ const UserProfile = () => {
     { tabName: "wishlist", title: "لسیت علاقه مندی ها" },
     { tabName: "orderHistory", title: "تاریخچه سفارشات" },
   ];
+
   return (
     <div className={`${styles.mainContainer}`}>
       <div className={`${styles.profile}`}>
@@ -48,31 +67,68 @@ const UserProfile = () => {
         </div>
       </div>
       <div className={`${styles.profileInfo}`}>
-        <Tab.Container id="left-tabs-example" defaultActiveKey="profile">
+        <Tab.Container id="left-tabs-example" defaultActiveKey="پروفایل">
           <Nav variant="dark" className="flex-row">
-            {tabs.map((item, index) => {
-              return (
-                <Nav.Item className="flex-row" key={index + 1}>
-                  <Nav.Link
-                    eventKey={item.tabName}
-                    title={item.tabName}
-                    onClick={setNavKey}
-                    className={
-                      item.tabName == key ? `${styles.borderBottom}` : null
-                    }
-                  >
-                    {item.title}
-                  </Nav.Link>
-                </Nav.Item>
-              );
-            })}
+            {width > 768 ? (
+              tabs.map((item, index) => {
+                return (
+                  <Nav.Item className="flex-row" key={index + 1}>
+                    <Nav.Link
+                      eventKey={item.title}
+                      title={item.title}
+                      onClick={setNavKey}
+                      className={
+                        item.title == key ? `${styles.borderBottom}` : null
+                      }
+                    >
+                      {item.title} 
+                    </Nav.Link>
+                  </Nav.Item>
+                );
+              })
+            ) : (
+              <NavDropdown title={key} id="nav-dropdown">
+                {tabs.map((item, index) => {
+                  return (
+                    <NavDropdown.Item
+                      key={index + 1}
+                      eventKey={item.title}
+                      onClick={setNavKey}
+                      title={item.title}
+                      className={
+                        item.title == key ? `${styles.borderBottom}` : null
+                      }
+                    >
+                      {item.title}
+                    </NavDropdown.Item>
+                  );
+                })}
+              </NavDropdown>
+            )}
           </Nav>
-
           <Tab.Content>
-            <Tab.Pane eventKey="profile">1 tab content</Tab.Pane>
-            <Tab.Pane eventKey="orders">2 tab content</Tab.Pane>
-            <Tab.Pane eventKey="wishlist">3 tab content</Tab.Pane>
-            <Tab.Pane eventKey="orderHistory">4 tab content</Tab.Pane>
+            <Tab.Pane eventKey="پروفایل">
+              {Object.keys(profile).map((value) => {
+                return (
+                  <InputGroup
+                    size="default"
+                    className={width > 768 ? "w-25 mb-3" : "w-100"}
+                  >
+                    <InputGroup.Text id="inputGroup-sizing-default">
+                      {profile[value]}
+                    </InputGroup.Text>
+                    <Form.Control
+                      placeholder={profile[value]}
+                      aria-label={value}
+                      aria-describedby="inputGroup-sizing-default"
+                    />
+                  </InputGroup>
+                );
+              })}
+            </Tab.Pane>
+            <Tab.Pane eventKey="سفارش ها">orders tab content</Tab.Pane>
+            <Tab.Pane eventKey="لیست علاقه مندی ها">3 tab content</Tab.Pane>
+            <Tab.Pane eventKey="تاریخچه سفارشات">4 tab content</Tab.Pane>
           </Tab.Content>
         </Tab.Container>
       </div>
---
+++ components/userProfile/userData.jsx
@@ -0,0 +1,9 @@
+export const profile = {
+        name:'نام',
+        lastname:'نام خانوادگی',
+        phone:'شماره',
+        email:'ایمیل',
+        address:'آدرس',
+        zipCode:'کد پستی',
+    }
+
